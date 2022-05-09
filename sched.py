71759339fd3d8a92318e99d949f7e72f66e27e7b
root <root@mlGrid1.fyre.ibm.com>
2022-05-09 11:12:22 +0100

    v1.0

--- sched.py
+++ sched.py
@@ -4,28 +4,14 @@ pd.options.display.max_rows = 9999      #output all rows in DF
 #print(pd.options.display.max_rows) 
 
 dfJS = pd.read_csv('srcuniqjs.csv')
-#print('dfJS.info()')  
 #print(dfJS.info())  
-#print('end of dfJS.info')
 dfJSD = pd.read_csv('srcuniqdepend.csv')
-#print('dfJSD.info()')  
 #print(dfJSD.info())  
-#print('end of dfJSD.info')
 
-#print ('dfJS.index=',dfJS.index)
-#print ('dfJSD.index=',dfJSD.index)
 #print('starting position:')
-#print ('JS',dfJS)
-#print ('JSD',dfJSD)
-#print('deal with BIR')
-for jsdindex,jsdrow in dfJSD.iterrows(): 
-		thisjs=jsdrow['JOBSTREAM']
-		if ((thisjs=='BIR_START_DAILY') or (thisjs=='BIR_SET_RUNDATE')):
-			rhjs=jsdrow['SUCCESSOR_JOBSTREAM']
-			dfJSD.at[jsdindex,'JOBSTREAM']=rhjs
-			dfJSD.at[jsdindex,'SUCCESSOR_JOBSTREAM']='xxx'
-print('dealt with bir')
-print('deal with rows in jsd where js is not in our master list')
+#print (dfJS)
+#print (dfJSD)
+#print('deal with rows in jsd where left js is not in our master list')
 for jsdindex,jsdrow in dfJSD.iterrows():
 	thisjsd=jsdrow['JOBSTREAM']
 	found=0
@@ -33,37 +19,38 @@ for jsdindex,jsdrow in dfJSD.iterrows():
 		thisjs=jsrow['JOBSTREAM']
 		if thisjs == thisjsd:
 			found=1
-	if (found == 0):
+	if (found == 0):          #not found in left so move right to the left
+		rhjs=jsdrow['SUCCESSOR_JOBSTREAM']
 		dfJSD.at[jsdindex,'JOBSTREAM']=rhjs
-		dfJSD.at[jsdindex,'SUCCESSOR_JOBSTREAM']='xxx'
-print('dealt with rows in jsd where js not in master list')
-print(dfJSD)
+		dfJSD.at[jsdindex,'SUCCESSOR_JOBSTREAM']='xxx' #put non-existent js in right
+#print('dealt with rows in jsd where js not in master list')
+#print(dfJSD)
 while True:
-	print ('numberToCheck=',len(dfJS))
+	#print ('numberToCheck=',len(dfJS))
 	if (len(dfJS)) == 0: 
 		quit()
-	print ('JS',dfJS)
-	print ('JSD',dfJSD)
+	#print ('JS',dfJS)
+	#print ('JSD',dfJSD)
 	for jsindex, jsrow in dfJS.iterrows():              #for each entry in list1
 		thisjs=jsrow['JOBSTREAM']
-		print("checking if this jobstream is on right=",thisjs)
+		#print("checking if this jobstream is on right=",thisjs)
 		foundOnRight=0
 		for jsdindex,jsdrow in dfJSD.iterrows():                  #check if the entry is on the right
 			#print("comparing with",dfJSD['SUCCESSOR_JOBSTREAM'][jsdindex])
 			if (thisjs == jsdrow['SUCCESSOR_JOBSTREAM']):
 				foundOnRight=1
-				print (thisjs,'found on the right')
+				#print (thisjs,'found on the right')
 		if (foundOnRight==0):
-			print('outputing this js next due on left',thisjs) #output it
+			#print('outputing this js next due on left') #output it
+			print(thisjs)
 			for jsdindex,jsdrow in dfJSD.iterrows():    #we have to shift left all JS on the right
 				if (thisjs==jsdrow['JOBSTREAM']):
 					rhjs=jsdrow['SUCCESSOR_JOBSTREAM']
 					if (rhjs != 'xxx'):
-						print('shifting left',rhjs)
+						#print('shifting left',rhjs)
 						dfJSD.at[jsdindex,'JOBSTREAM']=rhjs
 						dfJSD.at[jsdindex,'SUCCESSOR_JOBSTREAM']='xxx'
 					else:
 						dfJSD.drop(jsdindex,inplace=True)
 			dfJS.drop(jsindex, inplace=True) #wasnt found on the right and has been output, done with it
-print('Normal End')
 
